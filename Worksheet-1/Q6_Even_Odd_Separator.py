741fa14e4254db3151118429b28443de056ceee5
DandumaniGowtham <gowthamdandumani777@gmail.com>
2026-08-10 11:55:57 +0530

    Add even-odd separator function and input handling
    Implement a function to separate even and odd numbers from a list.

---
+++ Worksheet-1/Q6_Even_Odd_Separator.py
@@ -0,0 +1,18 @@
+def separate_even_odd(numbers):
+  even = []
+  odd = []
+  for num in numbers:
+    if num % 2 == 0:
+      even.append(num)
+    else:
+      odd.append(num)
+  return (even, odd)
+
+n = int(input("Enter size of list"))
+numbers = []
+for i in range(n):
+  numbers.append(int(input()))
+even, odd = separate_even_odd(numbers)
+print(even)
+print(odd)
+            
