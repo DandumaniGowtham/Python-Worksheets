61066fb638879b6a52a2d35f917fbf178418ac61
DandumaniGowtham <gowthamdandumani777@gmail.com>
2026-08-10 10:44:47 +0530

    Update Q1_Temperature_Conversion.py

--- Worksheet-1/Q1_Temperature_Conversion.py
+++ Worksheet-1/Q1_Temperature_Conversion.py
@@ -1,1 +1,5 @@
-
+def celsius_to_fahrenheit(celsius):
+  fahrenheit =  (celsius * 9/5) + 32
+  return fahrenheit
+celsius = float(input("Enter Temperature in Celsius:"))
+print(celsius_to_fahrenheit(celsius))
